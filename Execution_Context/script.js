680ad01570bfdedffd1480b86ff41ccc57e0aeec
pernimythri <pernimythri2005@gmail.com>
2026-01-31 11:06:24 +0530

    updated

---
+++ Execution_Context/script.js
@@ -0,0 +1,53 @@
+/* console.log(name) // Uncaught ReferenceError: Cannot access 'name' before initialization
+let name="Mythri" 
+console.log(name) // not print because of error (interpreter)  */ 
+
+/* 
+console.log(name) // Uncaught ReferenceError: Cannot access 'name' before initialization
+const name="Mythri" 
+console.log(name) // not print because of error (interpreter) */
+
+
+/* console.log(name)   // Mythri
+var name="Mythri" 
+console.log(name)   //Mythri */
+
+/* 
+// defaultly name is var 
+console.log(name)   // Mythri
+name="Mythri" 
+console.log(name)   //Mythri */
+
+
+// Without Functions
+/* let mythriAge = prompt("Mythri , waht is your age ? ")
+console.log(`Mythri your age is: ${mythriAge}`)
+console.log("Thank you for answering ")
+console.log("Please , subscribe to the channel")
+let vinayAge = prompt("Vinay , waht is your age ? ")
+console.log(`Vinay your age is: ${vinayAge}`)
+console.log("Thank you for answering ")
+console.log("Please , subscribe to the channel")
+let krishAge = prompt("krish , waht is your age ? ")
+console.log(`Krish your age is: ${krishAge}`)
+console.log("Thank you for answering ")
+console.log("Please , subscribe to the channel") */
+
+// With Functions
+/* let mythriAge = prompt("Mythri , waht is your age ? ")
+console.log(`Mythri your age is: ${mythriAge}`)
+functionMessage()
+let vinayAge = prompt("Vinay , waht is your age ? ")
+console.log(`Vinay your age is: ${vinayAge}`)
+functionMessage()
+let krishAge = prompt("krish , waht is your age ? ")
+console.log(`Krish your age is: ${krishAge}`)
+functionMessage()
+
+function functionMessage(){
+    console.log("Thank you for answering ")
+    console.log("Please , subscribe to the channel")
+} */
+
+
+    
